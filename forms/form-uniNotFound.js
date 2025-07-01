ef0f2255f8bf1baeca7d24cc113ee940dddb25be
Aasher <azadpanchi3689@gmail.com>
2025-07-01 22:26:11 +0500

    replaced google form (iframe) with own custom form

---
+++ forms/form-uniNotFound.js
@@ -0,0 +1,30 @@
+const updateFormOk = `<div>
+                        <h2>Your response has been recorded</h2>
+                        <p>We appreciate your contribution to Meritpoint!</p>
+                    </div>`;
+
+const updateFormBad = `<div>
+                         <h2>Something Went Wrong !!</h2>
+                         <p>Try agian!</p>
+                       </div>`;            
+
+const form = document.getElementById("form");
+form.addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    const request = await fetch("https://script.google.com/macros/s/AKfycbzWtmg3h_VbNGdsmMRBt5M_SCghp48Unm6pwD5zKwzvOGY8hTj0uyxT6Kx8CBW7xDCEBQ/exec",
+                                {method: "POST",
+                                 header: {"Content-Type": "application/json"},
+                                 body: JSON.stringify(data)}
+    );
+
+    const response = await request.text();
+    if (response === "200") {
+        form.innerHTML = updateFormOk;
+    } else {
+        form.innerHTML = updateFormBad;
+    }
+});
