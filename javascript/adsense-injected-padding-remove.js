0d11e06dcf18a0521d0c537f462736551cc7056f
Aasher <azadpanchi3689@gmail.com>
2025-08-04 14:52:09 +0500

    Update adsense-injected-padding-remove.js

--- javascript/adsense-injected-padding-remove.js
+++ javascript/adsense-injected-padding-remove.js
@@ -1,6 +1,13 @@
 
 // removing padding added by google ads
 
+const bodyStyles = getComputedStyle(document.body);
+if (bodyStyles.paddingTop !== "0px" || bodyStyles.paddingBottom !== "0px") {
+  document.body.style.paddingTop = "0px";
+  document.body.style.paddingBottom = "0px";
+}
+
+
 const observer = new MutationObserver(() => {
     if (document.querySelector("#anchor")) {
         document.body.style.paddingBottom = "0px";
